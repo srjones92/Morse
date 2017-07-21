f0a3a76b49e75bd33ed1e67321566d3897a7b928
Scott Jones <scott.r.jones@asu.eduZ>
2017-07-21 15:04:38 -0700

    added comment headers

---
+++ morse_modulation.m
@@ -0,0 +1,29 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%   morse_modulation
+%
+%   Modulates a given sequence onto a sinusoid
+%
+%   Inputs:      
+%       seqn: N x 1 in {-1, 0, 1} morse sequence of dash, space, dot
+%       fc: Carrier frequencey (Hz)
+%       fs: Sampling frequency (Hz)
+%
+%   Returns:
+%       Ys: 
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+function [Ys] = morse_modulation( seqn, fc, fs )
+
+
+
+
+
+
+
+
+
+
+end
