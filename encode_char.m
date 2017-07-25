e8786a96147bd6ae9dd34bc9c2aafad6d9518851
Scott Jones <scott.r.jones@asu.eduZ>
2017-07-25 13:34:13 -0700

    add encode_char, get morse_modulation working

---
+++ encode_char.m
@@ -0,0 +1,20 @@
+function dd = encode_char(ch)
+S = {morse_tree};
+D = {0}; %{''}
+while ~isempty(S)
+N = S{1};
+dd = D{1};
+S = S(2:end);
+D = D(2:end);
+if ~isempty(N)
+if N{1} == ch;
+    dd = dd(2:end);
+    return
+else
+S = {N{2} N{3} S{:}};
+D = {[dd, 1], [dd, -1], D{:}};
+end
+end
+end
+dd = 0;
+end
